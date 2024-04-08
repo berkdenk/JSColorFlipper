77c39961d8837027838acd5e7c445b67fb34db90
Berktuğ Denk <126001719+berkdenk@users.noreply.github.com>
2024-04-08 18:12:51 +0300

    First Commit

---
+++ ColorFlip/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ColorFlip project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ColorFlip.settings')
+
+application = get_asgi_application()
